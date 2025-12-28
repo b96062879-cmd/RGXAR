1ffa114bdb70794d6d08b1243b134cad53977595
b96062879-cmd <b96062879@gmail.com>
2025-12-28 22:31:49 +0300

    Delete config.js

--- config.js
+++
@@ -1,2 +0,0 @@
-// config.js (BU DOSYA .gitignore'a EKLENECEK)
-export const API_KEY = "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...";
